603209ce669de4ebd9c38b29fe2979670561e9ff
Juan Rivera <juanriv45@gmail.com>
2018-02-12 04:02:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -23,4 +23,4 @@ end
 #              f += 2
 #      return a
 #end
-For very small numbers (less than a million), trial division is the best way: divide by 2, 3, 5, and so on until the square root of the number. If you find a factor, the number is composite; otherwise, the number is prime
+#For very small numbers (less than a million), trial division is the best way: divide by 2, 3, 5, and so on until the square root of the number. If you find a factor, the number is composite; otherwise, the number is prime
