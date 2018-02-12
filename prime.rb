023a6f78ccbfc303a02fe4c3458903db4294c785
Juan Rivera <juanriv45@gmail.com>
2018-02-12 04:30:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
 # Add  code here!
 def prime?(test_num)
-  puts test_num
   i = test_num
-  puts i
   collection = []
   until i = 0 do
     if test_num % i === 0 then
       collection << i
-      i -= 1
     end
+    i -= 1
   end
   collection
 end
