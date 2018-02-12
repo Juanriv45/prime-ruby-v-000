75b432ee14ccff1b8119d6314617b0f1b9da5509
Juan Rivera <juanriv45@gmail.com>
2018-02-12 03:53:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,25 @@
 # Add  code here!
-def prime?(number)
-
+def prime?(array)
+  array.each |n| do
+    if trial_division(n) then
+      n
+    end
+  end
 end
+
+
+  def trial_division(n):
+      a = []  
+      while n%2 == 0:
+          a.append(2)
+          n/=2
+      f=3
+      while n > 1:
+          if (n % f == 0):
+              a.append(f)
+              n /= f
+          else:
+              f += 2             
+      return a
+end
+For very small numbers (less than a million), trial division is the best way: divide by 2, 3, 5, and so on until the square root of the number. If you find a factor, the number is composite; otherwise, the number is prime
