a1f7f3753e02af9f88367d55548827fbbefd60e6
Juan Rivera <juanriv45@gmail.com>
2018-02-12 04:06:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,18 +11,18 @@ def trial_division(number)
     number<4
 end
 
-# def trial_division(n):
-#      a = []
-#      while n%2 == 0:
-#          a.append(2)
-#          n/=2
-#      f=3
-#      while n > 1:
-#          if (n % f == 0):
-#              a.append(f)
-#              n /= f
-#          else:
-#              f += 2
-#      return a
-#end
+def trial_division(n):
+      a = []
+      while n%2 == 0:
+          a.append(2)
+          n/=2
+      f=3
+      while n > 1:
+          if (n % f == 0):
+              a.append(f)
+              n /= f
+          else:
+              f += 2
+      return a
+end
 #For very small numbers (less than a million), trial division is the best way: divide by 2, 3, 5, and so on until the square root of the number. If you find a factor, the number is composite; otherwise, the number is prime
