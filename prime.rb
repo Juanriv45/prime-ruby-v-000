610f4989fe7ad95c7a703983b7eafb736d0c06b0
Juan Rivera <juanriv45@gmail.com>
2018-02-12 04:43:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,15 @@ def prime?(test_num)
   i = test_num
   collection = []
   until i == 0
-    puts "Test:#{i}"
     if test_num % i === 0 then
       collection << i
     end
     i-=1
   end
 
-collection.length == 2
+if collection.length == 2
+  puts "#{test_num}"
+end
 
 
 end
