89be5d601ffcd08b1c9dbcc0f7ecb136513b5706
Juan Rivera <juanriv45@gmail.com>
2018-02-12 04:35:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,13 @@
 def prime?(test_num)
   i = test_num
   collection = []
-  until i = 0
-    #if test_num % i === 0 then
-    #  puts i
-      #collection << 1
-    puts i
-    i -= 1
+  until i == 0
+    if test_num % i === 0 then
+      puts i
+      collection << 1
+      i -= 1
+    end
+
   end
   collection
 end
